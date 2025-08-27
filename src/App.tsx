66c7bb4263ec0d90b3359d44e344d0e197a5d1a0
Sofia Orazklycheva <sofiya.fish02@mail.ru>
2025-08-27 17:21:12 +0300

    feat: add image

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,21 @@ import { RegisterForm } from './components/ui/form/RegisterForm'
 
 function App() {
   return (
-      <>
-          <div className="page__wrapper">
-              <div className="slider">
-                  <button className="slider__prev">←</button>
-                  <div className="page__image-wrapper">
-                      <img src="desert.jpg" className="page__image" alt="Пустыня"/>
-                  </div>
-                  <button className="slider__next">→</button>
+      <div className="pageWrapper">
+          <div className="contentWrapper">
+              <div className="pageImageWrapper">
+                  <img
+                      src="/images/mountain.jpg"
+                      className="pageImage"
+                      alt="Живописный горный пейзаж на фоне формы регистрации"
+                  />
               </div>
-              <div className="registration">
-                  <h1 className="registration__title">Создайте аккаунт</h1>
-                  <p className="registration__text">Уже есть аккаунт? <a className="registration__link"
-                                                                         href="">Войти</a></p>
+              <main>
                   <RegisterForm/>
-              </div>
-
+              </main>
           </div>
-      </>
+
+      </div>
   )
 }
 
